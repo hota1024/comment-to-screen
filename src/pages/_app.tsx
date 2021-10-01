1d459417ee9eab3a052bf6facbf480942c57e09d
hota1024 <hotalog@hota1024.com>
2021-10-02 01:24:58 +0900

    :tada: (=^・・^=) < init!

---
+++ src/pages/_app.tsx
@@ -0,0 +1,7 @@
+import type { AppProps } from 'next/app'
+
+function MyApp({ Component, pageProps }: AppProps): React.ReactElement {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import { NextPage } from 'next'
+import Head from 'next/head'
+
+/**
+ * HomePage component.
+ */
+export const HomePage: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>home - comment-to-zoom</title>
+      </Head>
+
+      <div>comment-to-zoom</div>
+    </>
+  )
+}
+
+export default HomePage
